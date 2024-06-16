a1610d446161dbcdff7a6196afa136f902a279c1
alynek <alyne0x67@gmail.com>
2024-06-16 10:48:05 -0300

    Create BinarySearch method and update .gitignore

---
+++ CSharp/hackerrank/BinarySearchClass.cs
@@ -0,0 +1,30 @@
+﻿namespace hackerrank
+{
+    public class BinarySearchClass
+    {
+        public int BinarySearch(List<int> array, int numero)
+        {
+            int inicio = 0;
+            int fim = array.Count - 1;
+
+            while (inicio <= fim)
+            {
+                var meio = (inicio + fim) / 2;
+
+                if (numero < array[meio])
+                {
+                    fim = meio - 1;
+                }
+                else if (numero > array[meio])
+                {
+                    inicio = meio + 1;
+                }
+                else
+                {
+                    return meio;
+                }
+            }
+            return -1;
+        }
+    }
+}
